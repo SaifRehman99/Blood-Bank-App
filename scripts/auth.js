f78b46e1bcec5a1af6a632a7cdc2b31b2a2a5685
SaifRehman99 <saifr7493@gmail.com>
2019-06-01 00:52:19 +0500

    Firebase initialized here

---
+++ scripts/auth.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCjFXqLQASDq_nhe-mcpI40iJXJS-7wb-I",
+    authDomain: "blood-bank-abbf8.firebaseapp.com",
+    databaseURL: "https://blood-bank-abbf8.firebaseio.com",
+    projectId: "blood-bank-abbf8",
+    storageBucket: "blood-bank-abbf8.appspot.com",
+    messagingSenderId: "224018960477",
+    appId: "1:224018960477:web:4462e92104505d02"
+  };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
