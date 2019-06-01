ce3b4d449b2afeead1ca3a60464d9b5a4bdad52e
SaifRehman99 <saifr7493@gmail.com>
2019-06-01 15:58:47 +0500

    msg div added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-LoginForm
+document.querySelector('#LoginForm').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    var a = document.querySelector('#loginEmail').value;
+    var b = document.querySelector('#loginPass').value;
+    console.log(a,b)
+})
+
+const alertMessage = (txt,cls) => {
+
+    
+
+}
