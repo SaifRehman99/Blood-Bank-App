b00d268071e35b4fb6cf0fa09fea7a69ec975c18
SaifRehman99 <saifr7493@gmail.com>
2019-06-03 19:08:41 +0500

    adding final touch

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ const printData = (data) => {
         let listData = item.data()
         output += `
           <tr class="table table-bordered">
-          <td>${id}</td>
+          <td id = 'uniqueID'>${id}</td>
             <td>${listData.Name}</td>
             <td>${listData.Address}</td>
             <td>${listData.Phone}</td>
@@ -28,12 +28,34 @@ const printData = (data) => {
 // getting the tapped result of donor here
 const getDataDonor = document.querySelector('#results');
 
-// adding listener here using event delegation here
-getDataDonor.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('btn')){
-        console.log('milgaya')
-    }
 
+// adding listener here using event delegation here
+getDataDonor.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('btn')) {
+
+        // getting the clicked item id here
+        // let element = e.target.parentElement.parentElement;
+        let element = document.querySelector('#uniqueID');
+
+        const makingArray = (element) => {
+            // making siblings array list here
+            let siblings = [];
+            let sibling = element.parentNode.firstChild;
+
+            // Loop through each sibling and push to the array
+            while (sibling) {
+                if (sibling.nodeType === 1 && sibling !== element) {
+                    siblings.push(sibling);
+                }
+                sibling = sibling.innerHTML;
+            }
+
+            return siblings;
+        }
+
+        console.log(makingArray(element))
+    }
 })
 
 // animation here
@@ -42,9 +64,8 @@ const init = () => {
     setTimeout(() => {
         // mainText.style.display = 'block';
         mainText.style.opacity = 1;
-        
+
     }, 1400);
 
 }
-
 init()
